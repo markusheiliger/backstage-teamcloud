2a8730e56beddfdb5f8cc7d395963f240c1d52ed
Markus Heiliger <markus.heiliger@dash-two.de>
2022-03-15 19:18:26 +0100

    ...

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,13 @@
 bin_dir=`cd "$(dirname "$BASH_SOURCE[0]")"; pwd`
 subscription_id="672fab42-9efc-4f65-9b3c-dd6144e4ef60"
 resource_group="backstage-$(whoami)"
+
 container_registry="teamcloud.azurecr.io"
 container_registry_username="TeamCloud"
 container_registry_password=$(az acr credential show --subscription 'b6de8d3f-8477-45fe-8d60-f30c6db2cb06' --resource-group 'TeamCloud-Registry' --name 'TeamCloud' --query 'passwords[0].value' -o tsv)
-image_name="teamcloud-dev/tcportal-backstage-$(whoami)"
+
+image_name_dev="teamcloud-dev/tcportal-backstage-$(whoami)"
+image_name_rel="teamcloud/tcportal-backstage"
 image_tag=$(date +%s)
 
 header() {
@@ -32,25 +35,38 @@ fi
 header "Logging into container registry $container_registry ..." \
 	&& docker login -u $container_registry_username --password-stdin $container_registry < <(echo $container_registry_password)
 
-header "Building image $image_name:$image_tag ..." \
-	&& docker build . -f packages/backend/Dockerfile --tag "$container_registry/$image_name:$image_tag" 
+header "Building image $image_name_dev:$image_tag ..." \
+	&& obsolteImages=$(docker images -q $container_registry/$image_name_dev) \
+	&& docker build . -f packages/backend/Dockerfile --tag "$container_registry/$image_name_dev:$image_tag" --tag "$container_registry/$image_name_dev:latest"
 
-header "Pushing image $image_name:$image_tag ..." \
-	&& docker push "$container_registry/$image_name:$image_tag" 
+[ ! -z "$obsolteImages" ] \
+	&& header "Cleaning up obsolete images ..." \
+	&& (docker image rm --force $obsolteImages || true) \
+	&& echo 'done'
+
+header "Pushing image $image_name_dev:$image_tag and :latest ..." \
+	&& docker push "$container_registry/$image_name_dev:$image_tag" \
+	&& docker push "$container_registry/$image_name_dev:latest" 	
 
 [ "$(az group exists --subscription $subscription_id --name $resource_group)" == "false" ] \
 	&& header "Creating resource group $resource_group ..." \
 	&& az group create --subscription $subscription_id --name $resource_group --location eastus -o none \
 	&& echo 'done'
 
-if [[ " $* " != *" nodeploy "* ]]; then
+if [[ " $* " == *" release "* ]]; then
+
+	header "Releasing image $container_registry/$image_name_dev:latest ..." \
+		&& docker tag $container_registry/$image_name_dev:latest $container_registry/$image_name_rel:latest \
+		&& docker push $container_registry/$image_name_rel:latest
+
+elif [[ " $* " == *" deploy "* ]]; then
 
 	header "Deploying to resource group $resource_group ..." \
 		&& az deployment group create --subscription $subscription_id --resource-group $resource_group --mode Complete --template-file ./resources/backstage.bicep -o none \
 			--parameters registryServer=$container_registry \
 			--parameters registryUsername=$container_registry_username \
 			--parameters registryPassword=$container_registry_password \
-			--parameters containerImage=$image_name:$image_tag \
+			--parameters containerImage=$image_name_dev:$image_tag \
 			--parameters teamcloudOrganizationName=Contoso \
 			--parameters azureClientId=d51c023e-dd7e-4ee3-a3ff-01f72bf135a6 \
 			--parameters azureClientSecret=sgm7Q~AyyVs4IKeXA9vQQDyjx7bwa0.lv72LR \
@@ -58,11 +74,11 @@ if [[ " $* " != *" nodeploy "* ]]; then
 
 else
 
-	header "Run container $container_registry/$image_name:$image_tag locally ..." \
-		&& ( [ ! -z "$(docker ps -a | grep backstage)" ] && docker container rm backstage -f || true ) \
-		&& docker run -d --name backstage -p 3000:3000 -p 7007:7007 \
-			$container_registry/$image_name:$image_tag \
-			node packages/backend --config app-config.yaml --config app-config.local.yaml \
+	header "Run container $container_registry/$image_name_dev:$image_tag locally ..." \
+		&& ( [ ! -z "$(docker ps -a | grep backstage)" ] && docker container rm backstage -f > /dev/null || true ) \
+		&& docker run -d --name backstage -p 7007:7007 \
+			$container_registry/$image_name_dev:$image_tag \
+			node packages/backend --config app-config.yaml --config app-config.local.yaml > /dev/null \
 		&& echo "done"		
 
 fi
--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,4 +1,6 @@
-import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
+import { useHotCleanup } from '@backstage/backend-common';
+import { CatalogBuilder, runPeriodically } from '@backstage/plugin-catalog-backend';
+import { MicrosoftGraphOrgEntityProvider, MicrosoftGraphOrgReaderProcessor } from '@backstage/plugin-catalog-backend-module-msgraph';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
@@ -6,9 +8,37 @@ import { PluginEnvironment } from '../types';
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
+
   const builder = await CatalogBuilder.create(env);
+  
+  const msGraphOrgEntityProvider = MicrosoftGraphOrgEntityProvider.fromConfig(
+    env.config,
+    {
+      id: 'https://graph.microsoft.com/v1.0',
+      target: 'https://graph.microsoft.com/v1.0',
+      logger: env.logger,
+    },
+  );
+
+  builder.addEntityProvider(msGraphOrgEntityProvider);
+
+  builder.addProcessor(
+    MicrosoftGraphOrgReaderProcessor.fromConfig(env.config, {
+      logger: env.logger,
+    }),
+  );
+
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  
   const { processingEngine, router } = await builder.build();
+  
   await processingEngine.start();
+  
+    // Trigger a read every 5 minutes
+    useHotCleanup(
+      module,
+      runPeriodically(() => msGraphOrgEntityProvider.read(), 5 * 60 * 1000),
+    );
+    
   return router;
 }
